d90559712fe3648023b77d914f913021aeae0089
SaiSivaSubramanian <38156517+saisivasubramanian@users.noreply.github.com>
2024-02-18 18:59:43 +0530

    Update streamlit_app.py
    new big change

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -40,22 +38,24 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-    
-
 except URLError as e:
   streamlit.error()
   
 # passing to troubleshoot
 streamlit.stop()
 
-#import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# snowflake related function
+def get_fruit_load_list:
+ with my_cnx.cursor() as my_cur:
+     my_cur.execute("select * from fruit_load_list")
+     return my_cur.fetchall()
+
+# add button to get list
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 # add text box
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
